91179f987149c3a8562ed5c9e01ed926bc04d81c
Mykhailo <mykhailo.davydenko.ik.2023@lpnu.ua>
2023-09-28 09:41:52 +0300

    comment z1

--- LR2/LR2.cpp
+++ LR2/LR2.cpp
@@ -13,14 +13,14 @@ int main()
 	double z2; // результат обчислення 2-го виразу  
 
 	cout << "a = "; cin >> a;
-
+	/*
 	z1 = cos(3 * Pi / 8 - a / 4) * cos(3 * Pi / 8 - a / 4) -
 		cos(11 * Pi / 8 + a / 4) * cos(11 * Pi / 8 + a / 4);
-
+		*/
 	z2 = sqrt(2) / 2 * sin(a / 2);
 
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
+	//cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
